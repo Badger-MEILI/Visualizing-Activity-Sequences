16f5277b103c3990924707392a34cc6b28a6d5d7
adi <acpr@kth.se>
2017-04-08 18:24:03 +0200

    first commit

---
+++ sankey_custom.js
@@ -0,0 +1,85 @@
+function getData() {
+    return {
+        "nodes": [{"node":52,"name":"H 1","xPos":1},{"node":66,"name":"H 2","xPos":2},{"node":42,"name":"H 3","xPos":3},{"node":67,"name":"H 4","xPos":4},{"node":29,"name":"H 5","xPos":5},{"node":34,"name":"P 5","xPos":5},{"node":5,"name":"P 6","xPos":6},{"node":12,"name":"H 6","xPos":6},{"node":65,"name":"H 7","xPos":7},{"node":28,"name":"P 7","xPos":7},{"node":50,"name":"W 7","xPos":7},{"node":6,"name":"B 8","xPos":8},{"node":74,"name":"P 8","xPos":8},{"node":51,"name":"W 8","xPos":8},{"node":41,"name":"H 8","xPos":8},{"node":15,"name":"W 9","xPos":9},{"node":54,"name":"V 9","xPos":9},{"node":64,"name":"P 9","xPos":9},{"node":13,"name":"B 9","xPos":9},{"node":70,"name":"H 9","xPos":9},{"node":37,"name":"P 10","xPos":10},{"node":2,"name":"B 10","xPos":10},{"node":23,"name":"W 10","xPos":10},{"node":0,"name":"z 10","xPos":10},{"node":59,"name":"H 10","xPos":10},{"node":68,"name":"W 11","xPos":11},{"node":4,"name":"z 11","xPos":11},{"node":57,"name":"H 11","xPos":11},{"node":33,"name":"B 11","xPos":11},{"node":48,"name":"P 11","xPos":11},{"node":46,"name":"W 12","xPos":12},{"node":3,"name":"z 12","xPos":12},{"node":40,"name":"P 12","xPos":12},{"node":1,"name":"B 12","xPos":12},{"node":18,"name":"O 12","xPos":12},{"node":53,"name":"D 13","xPos":13},{"node":56,"name":"O 13","xPos":13},{"node":36,"name":"W 13","xPos":13},{"node":63,"name":"P 13","xPos":13},{"node":26,"name":"O 14","xPos":14},{"node":32,"name":"V 14","xPos":14},{"node":43,"name":"D 14","xPos":14},{"node":49,"name":"W 14","xPos":14},{"node":58,"name":"H 14","xPos":14},{"node":35,"name":"R 14","xPos":14},{"node":45,"name":"P 14","xPos":14},{"node":7,"name":"V 15","xPos":15},{"node":25,"name":"O 15","xPos":15},{"node":21,"name":"H 15","xPos":15},{"node":60,"name":"P 15","xPos":15},{"node":20,"name":"W 15","xPos":15},{"node":14,"name":"P 16","xPos":16},{"node":16,"name":"W 16","xPos":16},{"node":9,"name":"H 16","xPos":16},{"node":73,"name":"P 17","xPos":17},{"node":19,"name":"W 17","xPos":17},{"node":69,"name":"H 17","xPos":17},{"node":55,"name":"H 18","xPos":18},{"node":61,"name":"P 18","xPos":18},{"node":8,"name":"W 18","xPos":18},{"node":39,"name":"H 19","xPos":19},{"node":24,"name":"W 19","xPos":19},{"node":30,"name":"P 19","xPos":19},{"node":10,"name":"H 20","xPos":20},{"node":17,"name":"W 20","xPos":20},{"node":72,"name":"P 20","xPos":20},{"node":62,"name":"W 21","xPos":21},{"node":44,"name":"P 21","xPos":21},{"node":27,"name":"H 21","xPos":21},{"node":47,"name":"H 22","xPos":22},{"node":11,"name":"P 22","xPos":22},{"node":22,"name":"H 23","xPos":23},{"node":31,"name":"P 23","xPos":23},{"node":38,"name":"P 24","xPos":24},{"node":71,"name":"H 24","xPos":24}],
+        "links": [{"source":52,"target":66,"value":7},{"source":66,"target":42,"value":7},{"source":42,"target":67,"value":7},{"source":67,"target":29,"value":6},{"source":67,"target":34,"value":1},{"source":34,"target":5,"value":1},{"source":29,"target":12,"value":6},{"source":12,"target":65,"value":3},{"source":12,"target":28,"value":1},{"source":5,"target":28,"value":1},{"source":12,"target":50,"value":2},{"source":50,"target":6,"value":1},{"source":28,"target":74,"value":2},{"source":50,"target":51,"value":1},{"source":65,"target":41,"value":3},{"source":41,"target":15,"value":1},{"source":74,"target":15,"value":1},{"source":41,"target":54,"value":1},{"source":51,"target":64,"value":1},{"source":74,"target":64,"value":1},{"source":6,"target":13,"value":1},{"source":41,"target":70,"value":1},{"source":64,"target":37,"value":2},{"source":13,"target":2,"value":1},{"source":15,"target":23,"value":2},{"source":54,"target":0,"value":1},{"source":70,"target":59,"value":1},{"source":23,"target":68,"value":2},{"source":0,"target":4,"value":1},{"source":59,"target":57,"value":1},{"source":2,"target":33,"value":1},{"source":37,"target":48,"value":2},{"source":68,"target":46,"value":2},{"source":4,"target":3,"value":1},{"source":48,"target":40,"value":2},{"source":33,"target":1,"value":1},{"source":57,"target":18,"value":1},{"source":40,"target":53,"value":1},{"source":3,"target":53,"value":1},{"source":18,"target":56,"value":1},{"source":46,"target":36,"value":2},{"source":1,"target":63,"value":1},{"source":40,"target":63,"value":1},{"source":56,"target":26,"value":1},{"source":53,"target":43,"value":1},{"source":36,"target":49,"value":2},{"source":63,"target":58,"value":1},{"source":53,"target":35,"value":1},{"source":63,"target":45,"value":1},{"source":35,"target":7,"value":1},{"source":32,"target":7,"value":1},{"source":26,"target":25,"value":1},{"source":58,"target":21,"value":1},{"source":45,"target":60,"value":1},{"source":49,"target":20,"value":2},{"source":43,"target":20,"value":1},{"source":20,"target":14,"value":1},{"source":60,"target":14,"value":2},{"source":20,"target":16,"value":2},{"source":21,"target":9,"value":1},{"source":25,"target":9,"value":1},{"source":7,"target":9,"value":1},{"source":14,"target":73,"value":2},{"source":16,"target":19,"value":1},{"source":9,"target":69,"value":3},{"source":16,"target":69,"value":1},{"source":69,"target":55,"value":4},{"source":73,"target":61,"value":2},{"source":19,"target":8,"value":1},{"source":55,"target":39,"value":4},{"source":8,"target":24,"value":1},{"source":61,"target":30,"value":2},{"source":30,"target":10,"value":1},{"source":39,"target":10,"value":4},{"source":24,"target":17,"value":1},{"source":30,"target":72,"value":1},{"source":17,"target":62,"value":1},{"source":72,"target":44,"value":1},{"source":10,"target":27,"value":5},{"source":27,"target":47,"value":5},{"source":62,"target":47,"value":1},{"source":44,"target":11,"value":1},{"source":47,"target":22,"value":7},{"source":11,"target":31,"value":1},{"source":31,"target":38,"value":1},{"source":22,"target":71,"value":6}]};
+	}
+
+var margin = {top: 1, right: 1, bottom: 6, left: 1},
+    width = 2000 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+
+var formatNumber = d3.format(",.0f"),
+    format = function(d) { return formatNumber(d) + " TWh"; },
+    color = d3.scale.category20();
+
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var sankey = d3.sankey()
+    .nodeWidth(5)
+    .nodePadding(5)
+    .size([width, height]);
+
+var path = sankey.link();
+
+var renderSankey = function(energy) {
+
+    console.log('rendering something');
+    console.log(energy);
+  window.width = 1800;
+  sankey
+      .nodes(energy.nodes)
+      .links(energy.links)
+      .layout(32);
+
+  var link = svg.append("g").selectAll(".link")
+      .data(energy.links)
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", path)
+      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+      .sort(function(a, b) { return b.dy - a.dy; });
+
+  link.append("title")
+      .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+  var node = svg.append("g").selectAll(".node")
+      .data(energy.nodes)
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+    .call(d3.behavior.drag()
+      .origin(function(d) { return d; })
+      .on("dragstart", function() { this.parentNode.appendChild(this); })
+      .on("drag", dragmove));
+
+  node.append("rect")
+      .attr("height", function(d) { return d.dy; })
+      .attr("width", sankey.nodeWidth())
+      .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
+      .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+    .append("title")
+      .text(function(d) { return d.name + "\n" + format(d.value); });
+
+  node.append("text")
+      .attr("x", -6)
+      .attr("y", function(d) { return d.dy / 2; })
+      .attr("dy", ".35em")
+      .attr("text-anchor", "end")
+      .attr("transform", null)
+      .text(function(d) { return d.name; })
+    .filter(function(d) { return d.x < width / 2; })
+      .attr("x", 6 + sankey.nodeWidth())
+      .attr("text-anchor", "start");
+
+  function dragmove(d) {
+    d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+    sankey.relayout();
+    link.attr("d", path);
+  }
+}
+
+renderSankey(getData());
